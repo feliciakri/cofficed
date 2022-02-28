6ff05e3b79d30fe04f34e6fd6978424e77ae1b44
lukmannm <luckymanlukman19@gmail.com>
2022-03-01 05:27:46 +0700

    vaccinde employee

--- src/pages/dashboard-employee/vaccine/index.tsx
+++ src/pages/dashboard-employee/vaccine/index.tsx
@@ -1,17 +1,38 @@
 import { Syringe, Upload } from "phosphor-react";
-import { Stepper, Button, Group, Modal, Text } from "@mantine/core";
-import React, { useState } from "react";
+import {
+  Stepper,
+  Button,
+  Group,
+  Modal,
+  Text,
+  Alert,
+  LoadingOverlay,
+} from "@mantine/core";
+import React, { useContext, useState } from "react";
 import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
+import { useMutation } from "react-query";
+import axios from "axios";
+import { AuthContext } from "../../../context/AuthContext";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const DUMMY_CERTIFICATE = [
   {
+    id: "",
+    dosis: "",
+    image: "",
     status: "verified",
   },
   {
+    id: "",
+    dosis: "",
+    image: "",
     status: "verified",
   },
   {
-    status: "not verified",
+    id: "",
+    dosis: "",
+    image: "",
+    status: "n",
   },
 ];
 
@@ -19,13 +40,53 @@ type ModalVaccineProps = {
   isOpened: boolean;
   setIsOpened: (arg: boolean) => void;
 };
+const postImage = async (data: any) => {
+  const { token, image } = data;
+  const { data: response } = await axios
+    .post(`${process.env.REACT_APP_API_KEY}/certificates/`, image, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(() => {})
+    .catch((err) => err);
 
+  return response.data;
+};
+
+type Inputs = {
+  image: string;
+};
 const ModalVaccine = ({ isOpened, setIsOpened }: ModalVaccineProps) => {
+  const { state } = useContext(AuthContext);
+  const { token } = state;
   const [isImage, setIsImage] = useState<string | null>(null);
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const image = isImage;
-    console.log(image);
+  const [isSucces, setIsSucces] = useState<boolean>(false);
+  const [isFailed, setIsFailed] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { register, handleSubmit } = useForm<Inputs>();
+  const mutation = useMutation(postImage, {
+    onMutate: () => {
+      setIsLoading(true);
+    },
+    onSuccess: async (data) => {},
+    onError: () => {
+      setIsLoading(false);
+      setIsFailed(true);
+      setTimeout(() => {
+        setIsFailed(false);
+      }, 2000);
+    },
+  });
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const bodyData = new FormData();
+    bodyData.append("image", data.image[0]);
+    const dataInput = {
+      token: token,
+      image: bodyData,
+    };
+    await mutation.mutate(dataInput);
   };
   const handleInputImage = (files: any) => {
     const file = files[0];
@@ -34,10 +95,16 @@ const ModalVaccine = ({ isOpened, setIsOpened }: ModalVaccineProps) => {
   };
   return (
     <Modal opened={isOpened} onClose={() => setIsOpened(false)} centered>
+      {isLoading && <LoadingOverlay visible={isLoading} />}
       <h1 className="text-center font-fraunces text-lg">
         Upload Certificate Vaccine
       </h1>
-      <form onSubmit={onSubmit} className="my-2">
+      {isFailed && (
+        <Alert title="Failed :(" color="red">
+          Please wait for the previous image verification...
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)} className="my-2">
         <Dropzone
           onDrop={handleInputImage}
           onReject={(files) => console.log("rejected files", files)}
@@ -48,6 +115,9 @@ const ModalVaccine = ({ isOpened, setIsOpened }: ModalVaccineProps) => {
             MIME_TYPES.svg,
           ]}
           maxSize={3 * 1024 ** 2}
+          {...register("image", {
+            required: true,
+          })}
         >
           {(status) => (
             <Group
