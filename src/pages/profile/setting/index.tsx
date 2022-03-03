0bf9bf89f33cc943fe0b6b7282e200424b9756e3
Felicia <akufelicia@gmail.com>
2022-03-03 08:44:08 +0700

    Bug fixes

--- src/pages/profile/setting/index.tsx
+++ src/pages/profile/setting/index.tsx
@@ -6,15 +6,17 @@ import {
 	Alert,
 	Group,
 	Text,
+	PasswordInput,
 } from "@mantine/core";
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthContext";
 import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
 import { useContext } from "react";
 import axios from "axios";
 import { useMutation, useQuery } from "react-query";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { AuthActionKind } from "../../../context/AuthReducer";
 
 const fetchProfile = async (token: string | null) => {
 	if (token) {
@@ -55,11 +57,22 @@ type ModalProps = {
 	setIsOpened: (arg: boolean) => void;
 };
 
-type Inputs = {
+type PasswordModalProps = {
+	isOpened: boolean;
+	setIsOpened: (arg: boolean) => void;
+	datap: any;
+};
+
+type InputImage = {
 	image: string;
 };
 
+type InputPassword = {
+	password: string;
+};
+
 const postAvatar = async (data: any) => {
+	alert(data);
 	const { token, image } = data;
 	const { data: response } = await axios
 		.post(`${process.env.REACT_APP_API_KEY}/users/avatar/`, image, {
@@ -76,19 +89,19 @@ const ModalAvatar = ({ isOpened, setIsOpened }: ModalProps) => {
 	const { state } = useContext(AuthContext);
 	const { token } = state;
 	const [isImage, setIsImage] = useState<string | null>(null);
-	const [isSucces, setIsSucces] = useState<boolean>(false);
+	const [isSuccess, setIsSuccess] = useState<boolean>(false);
 	const [isFailed, setIsFailed] = useState<boolean>(false);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
-	const { register, handleSubmit } = useForm<Inputs>();
+	const { register, handleSubmit } = useForm<InputImage>();
 	const mutation = useMutation(postAvatar, {
 		onMutate: () => {
 			setIsLoading(true);
 		},
 		onSuccess: async () => {
-			setIsSucces(true);
+			setIsSuccess(true);
 			setIsLoading(false);
 			setTimeout(() => {
-				setIsSucces(false);
+				setIsSuccess(false);
 			}, 2000);
 		},
 		onError: () => {
@@ -100,9 +113,10 @@ const ModalAvatar = ({ isOpened, setIsOpened }: ModalProps) => {
 		},
 	});
 
-	const onSubmit: SubmitHandler<Inputs> = async (data) => {
+	const onSubmit: SubmitHandler<InputImage> = async (data) => {
 		const bodyData = new FormData();
 		bodyData.append("image", data.image[0]);
+		console.log(bodyData);
 		const dataInput = {
 			token: token,
 			image: bodyData,
@@ -120,7 +134,7 @@ const ModalAvatar = ({ isOpened, setIsOpened }: ModalProps) => {
 		<Modal opened={isOpened} onClose={() => setIsOpened(false)} centered>
 			{isLoading && <LoadingOverlay visible={isLoading} />}
 			<h1 className="text-center font-fraunces text-lg">Upload Avatar</h1>
-			{isSucces && (
+			{isSuccess && (
 				<Alert title="Success!" color="Blue">
 					Your avatar has been uploaded
 				</Alert>
@@ -134,12 +148,7 @@ const ModalAvatar = ({ isOpened, setIsOpened }: ModalProps) => {
 				<Dropzone
 					onDrop={handleInputImage}
 					onReject={(files) => console.log("rejected files", files)}
-					accept={[
-						MIME_TYPES.png,
-						MIME_TYPES.jpeg,
-						MIME_TYPES.svg,
-						MIME_TYPES.svg,
-					]}
+					accept={[MIME_TYPES.png, MIME_TYPES.jpeg, MIME_TYPES.svg]}
 					maxSize={3 * 1024 ** 2}
 					{...register("image", {
 						required: true,
@@ -156,11 +165,15 @@ const ModalAvatar = ({ isOpened, setIsOpened }: ModalProps) => {
 									Drag images here or click to select files
 								</Text>
 								<Text size="sm" color="dimmed" inline mt={7}>
-									Attach as files as you like, each file
-									should not exceed 2mb
+									Attach your avatar, each file should not
+									exceed 2mb
 								</Text>
 								{isImage && (
-									<img src={isImage} alt="images vaccine" />
+									<img
+										className="mt-10"
+										src={isImage}
+										alt="images avatar"
+									/>
 								)}
 							</div>
 						</Group>
@@ -174,9 +187,84 @@ const ModalAvatar = ({ isOpened, setIsOpened }: ModalProps) => {
 	);
 };
 
+const ModalPassword = ({
+	isOpened,
+	setIsOpened,
+	datap,
+}: PasswordModalProps) => {
+	const { state } = useContext(AuthContext);
+	const { token } = state;
+	const [isSuccess, setIsSuccess] = useState<boolean>(false);
+	const [isFailed, setIsFailed] = useState<boolean>(false);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { register, handleSubmit } = useForm<InputPassword>();
+	const [value, setValue] = useState("");
+
+	const onSubmit: SubmitHandler<InputPassword> = async (data) => {
+		const dataInput = {
+			nik: datap.nik,
+			email: datap.email,
+			name: datap.name,
+			phone: datap.phone,
+			password: value,
+		};
+		await axios
+			.put(`${process.env.REACT_APP_API_KEY}/users/`, dataInput, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((res) => {
+				setIsSuccess(true);
+				setTimeout(() => {
+					setIsSuccess(false);
+				}, 2000);
+			})
+			.catch((err) => {
+				setIsFailed(true);
+				setTimeout(() => {
+					setIsFailed(false);
+				}, 2000);
+			});
+	};
+
+	return (
+		<Modal opened={isOpened} onClose={() => setIsOpened(false)} centered>
+			{isLoading && <LoadingOverlay visible={isLoading} />}
+			<h1 className="text-center font-fraunces text-lg">
+				Change Password
+			</h1>
+			{isSuccess && (
+				<Alert title="Success!" color="Blue">
+					Your password has been changed!
+				</Alert>
+			)}
+			{isFailed && (
+				<Alert title="Failed :(" color="red">
+					Password change failed
+				</Alert>
+			)}
+			<form onSubmit={handleSubmit(onSubmit)} className="my-2">
+				<PasswordInput
+					placeholder="Password"
+					label="Password"
+					required
+					value={value}
+					onChange={(event) => setValue(event.currentTarget.value)}
+				/>
+				<div className="flex justify-end mr-2 mt-6">
+					<Button type="submit">Submit</Button>
+				</div>
+			</form>
+		</Modal>
+	);
+};
+
 const ProfileSetting = () => {
+	const { dispatch } = useContext(AuthContext);
 	const { state } = useContext(AuthContext);
 	const { token, role } = state;
+	const navigate = useNavigate();
 	const isAdmin = role?.toLowerCase() === "admin";
 	const { isLoading, data } = useQuery("getProfile", () =>
 		fetchProfile(token)
@@ -185,8 +273,18 @@ const ProfileSetting = () => {
 	const [amOpened, setAmOpened] = useState(false);
 	//password modal
 	const [pwOpened, setPwOpened] = useState(false);
-	const theme = useMantineTheme();
-
+	//logout function
+	function LogOutHandler() {
+		localStorage.removeItem("token");
+		localStorage.removeItem("users");
+		dispatch({
+			type: AuthActionKind.LOGOUT,
+		});
+		const redirect = async () => {
+			await navigate("/");
+		};
+		redirect();
+	}
 	if (isLoading) {
 		return (
 			<>
@@ -197,12 +295,20 @@ const ProfileSetting = () => {
 		);
 	}
 	const { name, avatar, email, office_name, nik, phone } = data;
-	console.log(data);
 	return (
 		<>
 			<ModalAvatar isOpened={amOpened} setIsOpened={setAmOpened} />
+			<ModalPassword
+				isOpened={pwOpened}
+				setIsOpened={setPwOpened}
+				datap={data}
+			/>
 			<div className="divide-y divide-gray-200">
-				<div className="space-y-1">
+				<Button onClick={LogOutHandler} variant="outline" color="red">
+					Log Out
+				</Button>
+
+				<div className="mt-5 space-y-1">
 					<h3 className="text-lg leading-6 font-medium text-gray-900">
 						Profile
 					</h3>
@@ -286,18 +392,34 @@ const ProfileSetting = () => {
 						</div>
 						<div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-b sm:border-gray-200">
 							<dt className="text-sm font-medium text-gray-500">
-								Office
+								Phone
 							</dt>
 							<dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-								<span className="flex-grow">{office_name}</span>
-								<span className="ml-4 flex-shrink-0">
+								<span className="flex-grow">{phone}</span>
+								{/* <span className="ml-4 flex-shrink-0">
 									<button
 										type="button"
 										className="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
 									>
 										Update
 									</button>
-								</span>
+								</span> */}
+							</dd>
+						</div>
+						<div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-b sm:border-gray-200">
+							<dt className="text-sm font-medium text-gray-500">
+								Office
+							</dt>
+							<dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+								<span className="flex-grow">{office_name}</span>
+								{/* <span className="ml-4 flex-shrink-0">
+									<button
+										type="button"
+										className="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+									>
+										Update
+									</button>
+								</span> */}
 							</dd>
 						</div>
 						<div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-b sm:border-gray-200">
@@ -310,6 +432,7 @@ const ProfileSetting = () => {
 									<button
 										type="button"
 										className="bg-white rounded-md font-medium text-purple-600 hover:text-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+										onClick={() => setPwOpened(true)}
 									>
 										Update
 									</button>
