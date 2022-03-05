7e3aa8f64f65aad60b237bcc0a4cf35704c41391
Felicia <akufelicia@gmail.com>
2022-03-06 02:35:41 +0700

    Fixed sign in page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,19 @@
-import { Alert, Button, Input, PasswordInput } from "@mantine/core";
+import {
+	Alert,
+	Button,
+	Input,
+	InputWrapper,
+	PasswordInput,
+	Text,
+} from "@mantine/core";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { EnvelopeSimple, Key, WarningCircle } from "phosphor-react";
+import { Envelope, Key, WarningCircle } from "phosphor-react";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { AuthActionKind } from "../../context/AuthReducer";
 import { useNavigate } from "react-router-dom";
 import axios, { AxiosResponse } from "axios";
+import { ErrorMessage } from "@hookform/error-message";
 type InputLogin = {
 	identity: string;
 	password: string;
@@ -47,94 +55,110 @@ const Login = () => {
 
 	return (
 		<>
-			<div className="flex items-center bg-white h-screen">
-				<div className="flex w-screen md:flex-row">
-					<div className="flex items-center w-full justify-center p-6 sm:p-12 md:w-1/2">
-						<div className=" md:w-3/4">
+			<div className="min-h-screen bg-white flex">
+				<div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+					<div className="mx-auto w-full max-w-sm lg:w-96">
+						<div>
 							<img
-								src="/android-chrome-192x192.png"
+								className="h-20 w-auto"
+								src="/cofficed-logo-black.svg"
 								alt="logo"
-								className="h-16"
 							/>
-							<h1 className="mb-4 text-2xl lg:text-4xl font-bold text-black font-fraunces my-4">
+							<h1 className="mt-6 text-3xl font-fraunces text-gray-900">
 								Sign in to your account
 							</h1>
+						</div>
 
-							{isFailed && (
-								<Alert title="Failed!" color="red">
-									Failed to log in.
-								</Alert>
-							)}
-							<form
-								onSubmit={handleSubmit(onSubmit)}
-								className="my-2"
-							>
-								<div>
-									<label className="block text-sm">
-										Name
-									</label>
-									<Input
-										type="email"
-										placeholder="Your email"
-										icon={
-											<EnvelopeSimple className="w-6 h-6" />
-										}
-										{...register("identity", {
-											required: true,
-										})}
-										invalid={invalidEmail}
-									/>
-									{errors.identity && (
-										<p className="text-sm text-gray-500">
-											Please enter your email!
-										</p>
-									)}
+						<div className="mt-8">
+							<div className="mt-6">
+								{isFailed && (
+									<Alert title="Failed!" color="red">
+										Failed to log in.
+									</Alert>
+								)}
+								<form
+									onSubmit={handleSubmit(onSubmit)}
+									className="space-y-6"
+								>
+									<InputWrapper
+										id="input-email-label"
+										required
+										label="Email"
+									>
+										<Input
+											icon={<Envelope />}
+											placeholder="someone@mail.com"
+											invalid={invalidEmail}
+											{...register("identity", {
+												required: true,
+												pattern: {
+													value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+													message:
+														"Invalid email address",
+												},
+											})}
+										/>
+										<ErrorMessage
+											errors={errors}
+											render={({ message }) => (
+												<Text size="xs" color="red">
+													{message}
+												</Text>
+											)}
+											name="email"
+										/>
+									</InputWrapper>
+									<InputWrapper
+										id="input-password-label"
+										required
+										label="Password"
+										className="space-y-1"
+									>
+										<PasswordInput
+											icon={<Key />}
+											invalid={invalidPassword}
+											placeholder="●●●●●●●●●●"
+											{...register("password", {
+												required: true,
+												minLength: {
+													value: 8,
+													message:
+														"Password must be at least 8 characters",
+												},
+											})}
+										/>
+										<ErrorMessage
+											errors={errors}
+											render={({ message }) => (
+												<Text size="xs" color="red">
+													{message}
+												</Text>
+											)}
+											name="password"
+										/>
+									</InputWrapper>
+									<div className="mt-6 flex justify-end">
+										<Button size="sm" type="submit">
+											Login
+										</Button>
+									</div>
+								</form>
+								<div className="text-sm text-gray-500 my-4">
+									<p>
+										Need an account of forget your Password?
+									</p>
+									<p>Kindly contact your HR</p>
 								</div>
-								<div className="mt-4">
-									<label className="block text-sm">
-										Password
-									</label>
-									<PasswordInput
-										type="password"
-										placeholder="Your password"
-										icon={<Key className="w-6 h-6" />}
-										rightSection={
-											invalidPassword && (
-												<WarningCircle className="w-6 h-6 text-red-600" />
-											)
-										}
-										invalid={invalidPassword}
-										{...register("password", {
-											required: true,
-											minLength: 4,
-										})}
-									/>
-									{errors.password && (
-										<p className="text-sm text-gray-500">
-											Your password must be more than 4
-											characters
-										</p>
-									)}
-								</div>
-								<div className="mt-6 flex justify-end">
-									<Button size="sm" type="submit">
-										Login
-									</Button>
-								</div>
-							</form>
-							<div className="text-sm text-gray-500 my-4">
-								<p>Need an account of forget your Password?</p>
-								<p>Kindly contact your HR</p>
 							</div>
 						</div>
 					</div>
-					<div className="md:h-screen md:w-1/2 md:block hidden">
-						<img
-							className="object-cover w-full h-full"
-							src="https://source.unsplash.com/user/erondu/1600x900"
-							alt="images login"
-						/>
-					</div>
+				</div>
+				<div className="hidden lg:block relative w-0 flex-1">
+					<img
+						className="absolute inset-0 h-full w-full object-cover"
+						src="https://source.unsplash.com/user/erondu/1600x900"
+						alt="login background"
+					/>
 				</div>
 			</div>
 		</>
