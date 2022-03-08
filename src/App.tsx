9ec91eeffb84c7b125c11d821f8305913bc349c6
Felicia <akufelicia@gmail.com>
2022-03-08 08:33:24 +0700

    Typos and cosmetics

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,32 @@ import "react-date-range/dist/theme/default.css";
 import { NotificationsProvider } from "@mantine/notifications";
 
 function App() {
-  const queryClient = new QueryClient();
-  return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <MantineProvider
-            theme={{
-              breakpoints: {
-                xs: 500,
-                sm: 800,
-                md: 1000,
-                lg: 1200,
-                xl: 1400,
-              },
-            }}
-            emotionOptions={{ key: "mantine", prepend: false }}
-          >
-            <NotificationsProvider>
-              <Router />
-            </NotificationsProvider>
-          </MantineProvider>
-        </AuthContextProvider>
-      </QueryClientProvider>
-    </>
-  );
+	const queryClient = new QueryClient();
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				<AuthContextProvider>
+					<MantineProvider
+						theme={{
+							breakpoints: {
+								xs: 500,
+								sm: 800,
+								md: 1000,
+								lg: 1200,
+								xl: 1400,
+							},
+							fontFamily: "Questrial, sans-serif",
+						}}
+						emotionOptions={{ key: "mantine", prepend: false }}
+					>
+						<NotificationsProvider autoClose={10000}>
+							<Router />
+						</NotificationsProvider>
+					</MantineProvider>
+				</AuthContextProvider>
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default App;
--- src/components/Layout/Notifications/index.tsx
+++ src/components/Layout/Notifications/index.tsx
@@ -8,70 +8,70 @@ import { AuthContext } from "../../../context/AuthContext";
 import DefaultEmptyState from "../../EmptyStates";
 
 type NotificationsData = {
-  name: string;
-  created_at: string;
+	name: string;
+	created_at: string;
 };
 const Notifications = () => {
-  const { state } = useContext(AuthContext);
-  const { data, refetch } = useQuery("getNotifications", async () => {
-    if (state.token) {
-      const { data: response } = await axios.get(
-        `${process.env.REACT_APP_API_URL}/logcats/user`,
-        {
-          headers: {
-            Authorization: `Bearer ${state.token}`,
-          },
-        }
-      );
+	const { state } = useContext(AuthContext);
+	const { data, refetch } = useQuery("getNotifications", async () => {
+		if (state.token) {
+			const { data: response } = await axios.get(
+				`${process.env.REACT_APP_API_URL}/logcats/user`,
+				{
+					headers: {
+						Authorization: `Bearer ${state.token}`,
+					},
+				}
+			);
 
-      return response.data;
-    }
-  });
+			return response.data;
+		}
+	});
 
-  const [isDropdown, setIsDropdown] = useState<boolean>(false);
+	const [isDropdown, setIsDropdown] = useState<boolean>(false);
 
-  const handleDropdown = () => {
-    setIsDropdown(!isDropdown);
-    refetch();
-  };
-  return (
-    <div>
-      <div
-        className="lg:mx-10 bg-gray-200 p-2 rounded-full relative cursor-pointer"
-        onClick={handleDropdown}
-      >
-        <Bell size={25} />
-      </div>
-      <div
-        className={`${!isDropdown ? "hidden" : ""} max-h-92 absolute
+	const handleDropdown = () => {
+		setIsDropdown(!isDropdown);
+		refetch();
+	};
+	return (
+		<div>
+			<div
+				className="lg:mx-10 hover:bg-gray-200 p-2 rounded-full relative cursor-pointer"
+				onClick={handleDropdown}
+			>
+				<Bell size={25} />
+			</div>
+			<div
+				className={`${!isDropdown ? "hidden" : ""} max-h-92 absolute
 top-10 right-2 lg:right-10 bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4`}
-        role="menu"
-        aria-orientation="vertical"
-        aria-labelledby="menu-button"
-      >
-        <div className="px-4 py-3">
-          <h1>Notifications</h1>
-        </div>
-        <ul className="py-1" aria-labelledby="dropdown">
-          <ScrollArea style={{ height: 300 }}>
-            {!data && (
-              <div className="lg:w-1/2 mx-auto">
-                <DefaultEmptyState />
-              </div>
-            )}
-            {data?.map((notif: NotificationsData, i: number) => (
-              <li key={i} className=" px-4 py-2">
-                <p>{notif.name}</p>
-                <p className="text-sm text-blue-500">
-                  {moment(notif.created_at).fromNow()}
-                </p>
-              </li>
-            ))}
-          </ScrollArea>
-        </ul>
-      </div>
-    </div>
-  );
+				role="menu"
+				aria-orientation="vertical"
+				aria-labelledby="menu-button"
+			>
+				<div className="px-4 py-3">
+					<h1>Notifications</h1>
+				</div>
+				<ul className="py-1" aria-labelledby="dropdown">
+					<ScrollArea style={{ height: 300 }}>
+						{!data && (
+							<div className="lg:w-1/2 mx-auto">
+								<DefaultEmptyState />
+							</div>
+						)}
+						{data?.map((notif: NotificationsData, i: number) => (
+							<li key={i} className=" px-4 py-2">
+								<p>{notif.name}</p>
+								<p className="text-sm text-blue-500">
+									{moment(notif.created_at).fromNow()}
+								</p>
+							</li>
+						))}
+					</ScrollArea>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default Notifications;
