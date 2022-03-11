97034b9378e212b6740fbbe6efa9c684fb003f82
Felicia <akufelicia@gmail.com>
2022-03-11 07:09:19 +0700

    Updated animation

--- src/components/EmptyStates/index.tsx
+++ src/components/EmptyStates/index.tsx
@@ -1,15 +1,19 @@
-import { Center, Container, Space } from "@mantine/core";
-import Lottie from "lottie-react";
-import tumbleweedAnimation from "./tumbleweed.json";
+import { Center, Container, Space } from '@mantine/core'
+import Lottie from 'lottie-react'
+import tumbleweedAnimation from './tumbleweed.json'
 
 export default function DefaultEmptyState() {
-	return (
-		<Center>
-			<Container size="xs" padding="xs">
-				<Lottie animationData={tumbleweedAnimation} />
-				<Space h="sm" />
-				<Center>Found Nothing Here</Center>
-			</Container>
-		</Center>
-	);
+    return (
+        <Center>
+            <Container size="xs" padding="xs">
+                <Lottie
+                    animationData={tumbleweedAnimation}
+                    loop={true}
+                    autoplay={true}
+                />
+                <Space h="sm" />
+                <Center>Found Nothing Here</Center>
+            </Container>
+        </Center>
+    )
 }
